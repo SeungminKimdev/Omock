02388518430b1e9408eeb6e9a786be881feda89e
SeungminKimdev <ksm7250432@gmail.com>
2025-08-02 18:35:23 +0900

    feat: add CORS configuration

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,25 @@
 import models
 from database import Base, engine
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"]
+)
 
 app.include_router(router)
 
